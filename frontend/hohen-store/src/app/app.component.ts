1561b9b4297048244d838c9ff1a561d33b563739
Medo-02 <102298394+Medo-02@users.noreply.github.com>
2025-04-20 11:30:24 +0300

    Init Angular project

---
+++ frontend/hohen-store/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'hohen-store';
+  
+  stateOptions: any[] = [
+    { label: 'Option 1', value: 'option1' },
+    { label: 'Option 2', value: 'option2' },
+    { label: 'Option 3', value: 'option3' }
+  ];
+
+  value: string = 'option1';
+}
---
+++ frontend/hohen-store/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+          preset: Aura,
+        }
+    })
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
