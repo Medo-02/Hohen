2b85fd9011354bd84a106396456796cc28b18219
Medo-02 <102298394+Medo-02@users.noreply.github.com>
2025-04-20 11:38:43 +0300

    Create Product model

--- frontend/hohen-store/src/app/app.module.ts
+++ frontend/hohen-store/src/app/app.module.ts
@@ -7,16 +7,18 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { ProductListComponent } from "./Components/product-list/product-list.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
-  ],
+    FormsModule,
+    ProductListComponent
+],
   providers: [
     provideAnimationsAsync(),
     providePrimeNG({
