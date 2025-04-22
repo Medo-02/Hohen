f4e6d099833393b1d2649a1a968e06639a85246f
Medo-02 <102298394+Medo-02@users.noreply.github.com>
2025-04-22 07:34:37 +0300

    Create new entities
    Introduce Cutsomer - Address - Order - - OrderItem entites

--- backend/src/main/java/com/hohenheim/store/dao/ProductRepository.java
+++ backend/src/main/java/com/hohenheim/store/dao/ProductRepository.java
@@ -14,5 +14,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource(excerptProjection = ProductWithImages.class)
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    // Example ..search/findByCategoryId?id=1
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    // Example search/findByNameContaining?name=Java&page=0&size=20'
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
+
 }
---
+++ backend/src/main/java/com/hohenheim/store/entity/OrderItem.java
@@ -0,0 +1,89 @@
+package com.hohenheim.store.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "unit_price")
+    private Long unitPrice;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(Long unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "id=" + id +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", quantity=" + quantity +
+                ", productId=" + productId +
+                ", order=" + order +
+                '}';
+    }
+}
