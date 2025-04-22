11e80709adeca614bd2a1a68d4ed619166963b19
Medo-02 <102298394+Medo-02@users.noreply.github.com>
2025-04-22 04:56:29 +0300

    Create endpoint for search by category id

--- backend/src/main/java/com/hohenheim/store/dao/ProductRepository.java
+++ backend/src/main/java/com/hohenheim/store/dao/ProductRepository.java
@@ -2,7 +2,10 @@ package com.hohenheim.store.dao;
 
 import com.hohenheim.store.entity.Product;
 import com.hohenheim.store.projection.ProductWithImages;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -10,4 +13,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(excerptProjection = ProductWithImages.class)
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
