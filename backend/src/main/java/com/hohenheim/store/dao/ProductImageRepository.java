27d51ea799b32c99fab4409b775da3d5f395a254
Medo-02 <102298394+Medo-02@users.noreply.github.com>
2025-04-22 04:06:49 +0300

    Refactor the product entity
    Instead of holding one image now the product can have several images where it can be combined with the product entity using Projection (DTO) which helps in embedding the images into the json response

--- backend/src/main/java/com/hohenheim/store/dao/ProductImageRepository.java
+++ backend/src/main/java/com/hohenheim/store/dao/ProductImageRepository.java
@@ -1,11 +1,12 @@
 package com.hohenheim.store.dao;
 
-import com.hohenheim.store.entity.ProductCategory;
+
+import com.hohenheim.store.entity.ProductImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
-@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+@RepositoryRestResource
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
 }
--- backend/src/main/java/com/hohenheim/store/dao/ProductRepository.java
+++ backend/src/main/java/com/hohenheim/store/dao/ProductRepository.java
@@ -1,12 +1,13 @@
 package com.hohenheim.store.dao;
 
 import com.hohenheim.store.entity.Product;
+import com.hohenheim.store.projection.ProductWithImages;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 // accept calls from web browser
 @CrossOrigin("http://localhost:4200")
-@RepositoryRestResource
+@RepositoryRestResource(excerptProjection = ProductWithImages.class)
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
