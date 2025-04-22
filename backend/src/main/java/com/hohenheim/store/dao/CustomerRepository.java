501ea4ac52c3f05de897985aa03085a1b1f1a4ca
Medo-02 <102298394+Medo-02@users.noreply.github.com>
2025-04-22 08:11:50 +0300

    Create the place order functionality
    All customer and order info are grouped inside the purchase DTO which then used in the checkout service that
     - generate the tracking number
    - fill in order info and customer info using the purchase object
    - save data to the database

---
+++ backend/src/main/java/com/hohenheim/store/dao/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.hohenheim.store.dao;
+
+import com.hohenheim.store.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ backend/src/main/java/com/hohenheim/store/dto/Purchase.java
@@ -0,0 +1,68 @@
+package com.hohenheim.store.dto;
+
+import com.hohenheim.store.entity.Address;
+import com.hohenheim.store.entity.Customer;
+import com.hohenheim.store.entity.OrderItem;
+import com.hohenheim.store.entity.Order;
+
+import java.util.Set;
+
+public class Purchase {
+
+    private Customer customer;
+    private Address shippingAddress;
+    private Address billingAddress;
+    private Order order;
+    private Set<OrderItem> orderItems;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public Address getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(Address billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Set<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(Set<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+
+    @Override
+    public String toString() {
+        return "Purchase{" +
+                "customer=" + customer +
+                ", shippingAddress=" + shippingAddress +
+                ", billingAddress=" + billingAddress +
+                ", order=" + order +
+                ", orderItems=" + orderItems +
+                '}';
+    }
+}
