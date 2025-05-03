ba188ae1d77f76164cfc4fa6272d912aaf0baf8c
Medo-02 <102298394+Medo-02@users.noreply.github.com>
2025-05-03 20:02:22 +0300

    Add interceptor for auth only APIs

---
+++ frontend/hohen-store/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable, inject } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent
+} from '@angular/common/http';
+import { Observable, from } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import Keycloak from 'keycloak-js';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    private keycloak = inject(Keycloak); 
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (!req.url.includes('/api')) {
+            return next.handle(req);
+        }
+        return from(this.keycloak.tokenParsed ? Promise.resolve(this.keycloak.token!) : this.keycloak.updateToken(5).then(() => this.keycloak.token!)).pipe(
+            switchMap(token => {
+                if (token) {
+                const authReq = req.clone({
+                    setHeaders: {
+                    Authorization: `Bearer ${token}`
+                    }
+                });
+                return next.handle(authReq);
+                }
+                return next.handle(req);
+            })
+        );
+  }
+}
--- frontend/hohen-store/src/main.ts
+++ frontend/hohen-store/src/main.ts
@@ -3,12 +3,13 @@ import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import routeConfig from './app/routes';
 import MyPreset from './custom-preset';
 import { provideKeycloakAndInterceptor } from './app/interceptors/KeycloakAndInterceptor';
-
+import { withInterceptorsFromDi } from '@angular/common/http';
+import { AuthInterceptor } from './app/interceptors/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -19,8 +20,10 @@ bootstrapApplication(AppComponent, {
       },
       ripple: true,
     }),
-    provideHttpClient(),
+        provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     provideRouter(routeConfig),
     provideKeycloakAndInterceptor()
+    
   ],
 }).catch((err) => console.error(err));
