907d42ef577f9b512f9812521a069168a3e92201
Medo-02 <102298394+Medo-02@users.noreply.github.com>
2025-05-06 17:50:10 +0300

    Create DTO to get category name and id

--- backend/src/main/java/com/hohenheim/store/dao/ProductCategoryRepository.java
+++ backend/src/main/java/com/hohenheim/store/dao/ProductCategoryRepository.java
@@ -1,10 +1,18 @@
 package com.hohenheim.store.dao;
 
+import com.hohenheim.store.dto.CategoryDTO;
 import com.hohenheim.store.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@RestResource(path = "findCategoryIdsAndNames", rel = "findCategoryIdsAndNames")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+    @Query("SELECT new com.hohenheim.store.dto.CategoryDTO(c.id, c.categoryName) FROM ProductCategory c")
+    List<CategoryDTO> findCategoryIdsAndNames();
 }
