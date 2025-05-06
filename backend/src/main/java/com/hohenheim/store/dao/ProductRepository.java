4552990821cb7e00ce356cd8a288bba091d05b3c
Medo-02 <102298394+Medo-02@users.noreply.github.com>
2025-05-06 23:16:36 +0300

    Squashed commit of the following:
    commit 1e70c70c80bcc6c0c607007bcf4eb2d2608742e2
    Author: Medo-02 <102298394+Medo-02@users.noreply.github.com>
    Date:   Tue May 6 23:13:10 2025 +0300
    
        Add get categories to product
    
    commit 907d42ef577f9b512f9812521a069168a3e92201
    Author: Medo-02 <102298394+Medo-02@users.noreply.github.com>
    Date:   Tue May 6 17:50:10 2025 +0300
    
        Create DTO to get category name and id
    
    commit 1d65146f8aeb0302738511ad72eb2f7521528a96
    Author: Medo-02 <102298394+Medo-02@users.noreply.github.com>
    Date:   Mon May 5 16:31:20 2025 +0300
    
        Create docker container to run backend and db
    
    commit 38d4060a9dd0d15dafd2389d8ebd3d0c733cd0d0
    Author: Medo-02 <102298394+Medo-02@users.noreply.github.com>
    Date:   Fri May 2 14:12:53 2025 +0300
    
        Refactor the configirations
    
    commit fce5bf1f41d4b78e5bd1f495e11743c2ceac69fc
    Author: Medo-02 <102298394+Medo-02@users.noreply.github.com>
    Date:   Fri May 2 12:54:06 2025 +0300
    
        Add key cloak jwt key
    
    commit feafa785227badf195bf16d4c705939325fbbc09
    Author: Medo-02 <102298394+Medo-02@users.noreply.github.com>
    Date:   Thu May 1 10:50:03 2025 +0300
    
        Refactor Security to use OAuth2
    
    commit 0e61a2000ab081c2f027f4eeb68d7f907829c9c5
    Author: Medo-02 <102298394+Medo-02@users.noreply.github.com>
    Date:   Tue Apr 29 10:49:15 2025 +0300
    
        Update login endpoint to use JWT service
    
    commit 4ab3835826e53661a46a07c9db4f0ab2acbaccfc
    Author: Medo-02 <102298394+Medo-02@users.noreply.github.com>
    Date:   Tue Apr 29 10:45:42 2025 +0300
    
        Refactor JWT generator to use service
    
    commit e68c14c9e962aecc27fa58be12a381cadc538e24
    Author: Medo-02 <102298394+Medo-02@users.noreply.github.com>
    Date:   Tue Apr 29 10:21:42 2025 +0300
    
        Create Login endpoint
    
    commit d9804d4a621eebd4e499187967ff8890d38f9954
    Author: Medo-02 <102298394+Medo-02@users.noreply.github.com>
    Date:   Tue Apr 29 08:38:27 2025 +0300
    
        Create JWT logic generation + validation
    
    commit 30fc570babecd6a5b8eec39626f861d2fb6f9a41
    Author: Medo-02 <102298394+Medo-02@users.noreply.github.com>
    Date:   Tue Apr 29 07:15:45 2025 +0300
    
        Add custom filter to log authenticated user
    
    commit 7589b1bd595ef988ec890f318761cdeacd68e1ab
    Author: Medo-02 <102298394+Medo-02@users.noreply.github.com>
    Date:   Mon Apr 28 09:24:28 2025 +0300
    
        Use global Cors configiration

--- backend/src/main/java/com/hohenheim/store/dao/ProductRepository.java
+++ backend/src/main/java/com/hohenheim/store/dao/ProductRepository.java
@@ -5,16 +5,18 @@ import com.hohenheim.store.projection.ProductWithImages;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-// accept calls from web browser
+import java.util.List;
+
 @RepositoryRestResource(excerptProjection = ProductWithImages.class)
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    // Example ..search/findByCategoryId?id=1
-    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+    @Query("SELECT p FROM Product p WHERE p.category.id IN :categories")
+    Page<Product> findByCategoryIds(@Param("categories") List<Long> categoryIds, Pageable pageable);
 
     // Example search/findByNameContaining?name=Java&page=0&size=20'
     Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
